aba11f89126440d0f437f424238fb616c0ab4b59
yongkheehou <yongkheehou@gmail.com>
2022-08-07 18:22:21 +0800

    new

--- prepare.py
+++ prepare.py
@@ -1,3 +1,25 @@
 import numpy as np
+import pandas as pd
 
-print(1)
+download_link = ("https://raw.githubusercontent.com/yongkheehou/recyclegowheredata/main/db.csv")
+
+class PrepareData:
+    
+    def __init__(self, download_new=True):
+        self.download_new = download_new
+        
+    def download_data(self):
+        df = pd.read_csv(download_link)
+        return df
+    
+    def write_data(self, data, directory, **kwargs):
+        for name, df in data.items():
+            df.to_csv(f"{directory}/{name}.csv", **kwargs)
+            
+    def read_local_data(self, group, kind):
+        name = f"{group}_{kind}"
+        return pd.read_csv(f"data/raw/{name}.csv")
+
+# working code
+# x = pd.read_csv(download_link)
+# print(x.head)
